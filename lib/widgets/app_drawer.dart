b949dc1910728e68510b0c79161d7d3924ae2547
Faisal Karim <Faisalmohammed2002@gmail.com>
2021-07-14 15:40:41 +1000

    Connected to firebase and animated

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/helpers/custom_route.dart';
+import 'package:flutter_shop/providers/auth_provider.dart';
 import 'package:flutter_shop/screens/orders_screen.dart';
+import 'package:flutter_shop/screens/user_products_screen.dart';
+import 'package:provider/provider.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key key}) : super(key: key);
@@ -25,6 +29,21 @@ class AppDrawer extends StatelessWidget {
             onTap: () => Navigator.of(context)
                 .pushReplacementNamed(OrdersScreen.routeName),
           ),
+          ListTile(
+            leading: const Icon(Icons.edit),
+            title: const Text('Manage Products'),
+            onTap: () => Navigator.of(context)
+                .pushReplacementNamed(UserProductsScreen.routeName),
+          ),
+          const Divider(),
+          ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Logout'),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pushReplacementNamed('/');
+                Provider.of<AuthProvider>(context, listen: false).signout();
+              }),
         ],
       ),
     );
